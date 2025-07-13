ba9bafccbd5e3031b9b3bbdbedfb73b3e8b48b69
zubairchamp <zubairChamp123@gmail.com>
2025-07-13 13:55:25 +0100

    why

---
+++ day 3/activity3.py
@@ -0,0 +1,8 @@
+print("enters marks of optint in 3 subjects")
+math=int(input("maths "))
+english=int(input("english "))
+science=int(input("science "))
+
+sum=math+english+science
+percentage=(sum/300)*100
+print(percentage)
