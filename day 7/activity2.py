0a179313490e4f16cfa13365f66836e1de02ba55
zubairchamp <zubairChamp123@gmail.com>
2025-08-07 13:15:49 +0100

    okddd

---
+++ day 7/activity2.py
@@ -0,0 +1,6 @@
+num1=int(input("enter numerator"))
+num2=int(input("enter denominator"))
+if num1%num2== 0:
+    print("numbers are divisble")
+else:
+    print("numbers are not divisble")
---
+++ day 7/activity3.py
@@ -0,0 +1,8 @@
+mean=38
+correct_number=56
+wrong_number=36
+total_number=40
+sum=mean*total_number
+num2=sum-((wrong_number)-(correct_number))
+correct_mean=num2/total_number
+print(correct_mean)
