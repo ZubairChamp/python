1fb91e8d74de259156bd50f17f95445ce1f9b210
zubairchamp <zubairChamp123@gmail.com>
2025-08-19 13:22:22 +0100

    loop

---
+++ day 9/activity2.py
@@ -0,0 +1,7 @@
+string1=input("please enter your own string: ")
+string2=""
+for i in string1:
+    string2=i+string2
+print("\n the original string is ",string1)
+print("\n the reversed string is ",string2)
+
