8978be5617bb923c9973513afbeedfa0e9fb5588
zubairchamp <zubairChamp123@gmail.com>
2025-09-28 13:23:13 +0100

    zip,exit

---
+++ day24/activity2.py
@@ -0,0 +1,4 @@
+name=["'zubair","rayyan","khalil","iman"]
+rollnumber=[1,2,3,4]
+mapped=zip(name,rollnumber)
+print(list(mapped))         
---
+++ day24/activity3.py
@@ -0,0 +1,8 @@
+ages=[12,21,1722,1]
+for age in ages:
+    if age<18:
+        print(age," not allowed ")
+        print(exit)
+        exit()
+    else:
+        print(age," allowed ")
